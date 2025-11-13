861ffd9dfb0f5a21854213d702076191488d9935
ishanrwt <rawatishan51@gmail.com>
2025-11-13 22:58:06 +0530

    added models for tags

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
+class Tag(models.Model):
+    label=models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    tag=models.ForeignKey(Tag,on_delete=models.CASCADE)
+    ##here we didnt exported the products directly in the tagged item because then the tagged app 
+    #will  become dependent on the store app which we dont want 
+    content_type=models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id=models.PositiveIntegerField()
+    content_object=GenericForeignKey()
