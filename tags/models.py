3ec3f23b672067cd09b3f8cc30a33d37aa09834b
ishanrwt <rawatishan51@gmail.com>
2025-11-16 04:38:48 +0530

    tag features

--- tags/models.py
+++ tags/models.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 class Tag(models.Model):
     label=models.CharField(max_length=255)
+    tag_name=models.CharField(max_length=255)
 
 
 class TaggedItem(models.Model):
@@ -12,4 +13,5 @@ class TaggedItem(models.Model):
     #will  become dependent on the store app which we dont want 
     content_type=models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id=models.PositiveIntegerField()
+
     content_object=GenericForeignKey()
